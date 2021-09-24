1ebd88c78cb7e766aa7c1d23fe4bab86d81d0050
Katie <katie.herda2@gmail.com>
2021-09-24 02:15:09 +0000

    created callback hell, chaining together all previous functions and calling them under one callback.

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -15,4 +32,14 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 //     return;
 //   }
 //   console.log('It worked! Return coordinates: ', coordinates);
+// });
+
+// const coordinates = { latitude: 51.0207, longitude: -114.1011 }
+
+// fetchISSFlyOverTimes(coordinates, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Return flyover times: ', passTimes);
 // });
--- iss.js
+++ iss.js
@@ -1,12 +1,37 @@
+const request = require('request');
+
 /**
- * Makes a single API request to retrieve the user's IP address.
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
  * Input:
- *   - A callback (to pass back an error or the IP string)
+ *   - A callback with an error or results.
  * Returns (via Callback):
  *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
  */
-const request = require('request');
+
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
+
 
 //Function that fetches an IP address
 const fetchMyIP = function(callback) {
@@ -24,12 +49,9 @@ const fetchMyIP = function(callback) {
       return callback(Error(msg), null);
     }
 
-    const data = JSON.parse(body);
+    const ip = JSON.parse(body).ip;
     //extract just the data, not the full object
-    callback(null, data.ip);
-
-    //confirm IP address is a string
-    //console.log(typeof data.ip);
+    callback(null, ip);
   });
 };
 
@@ -48,10 +70,30 @@ const fetchCoordsByIP = function(ip, callback) {
     }
 
     //data restructuring
-    const {longitude, latitude} = JSON.parse(body);
+    const {latitude, longitude} = JSON.parse(body);
   
-    callback(null, {longitude, latitude});
+    callback(null, {latitude, longitude});
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+//Function the fetches ISS flyovers for a given location
+const fetchISSFlyOverTimes = function(coordinates, callback) {
+  request(`https://iss-pass.herokuapp.com/json/?lat=${coordinates.latitude}&lon=${coordinates.longitude}`, (error, response, body) => {
+
+    if (error) {
+      return callback(error, null);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching ISS pass time. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+
+  });
+};
+
+// Only export nextISSTimesForMyLocation and not the other functions, they are not needed by external modules.
+module.exports = { nextISSTimesForMyLocation };
